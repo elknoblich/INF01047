a87929b62b396ac8fffd77a85b192dd8ee95d8f9
Nicolas A.N.S. <nicolas.andre263@protonmail.com>
2024-08-03 13:34:42 -0300

    Teste de intersecção AABB

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -2,6 +2,8 @@
 #include <glm/vec4.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
+// AABB em coordenadas globais
 class AABB {
 
    private:
@@ -20,3 +22,12 @@ class AABB {
    glm::vec3 get_min() { return bbox_min; };
    glm::vec3 get_max() { return bbox_max };
 };
+
+bool AABB_to_AABB_intersec(AABB aabb1, AABB aabb2) {
+   glm::vec3 aabb_min_1 = aabb1.get_min();
+   glm::vec3 aabb_max_1 = aabb1.get_max();
+   glm::vec3 aabb_min_2 = aabb1.get_min();
+
+   return (aMin.x <= bMax.x && aMax.x >= bMin.x) && (aMin.y <= bMax.y && aMax.y >= bMin.y) && (aMin.z <= bMax.z && aMax.z >= bMin.z);
+}
+}
