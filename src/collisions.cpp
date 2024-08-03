897c9c61a33ac03c52a2b5cf96a965f9332d40c4
Nicolas A.N.S. <nicolas.andre263@protonmail.com>
2024-08-03 15:06:50 -0300

    correção

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -1,45 +1,35 @@
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-
+#include "../include/collisions.hpp"
 // AABB em coordenadas globais
-class AABB {
-
-   private:
-   glm::vec3 bbox_min; // Axis-Aligned Bounding Box do objeto
-   glm::vec3 bbox_max;
-   glm::mat4 model;
-
-
-   public:
-   AABB(glm::vec3 bbox_min_local, glm::vec3 bbox_max_local, glm::mat4 model) {
-      glm::vec4 bbox_min_global = model * glm::vec4(bbox_min_local.x, bbox_min_local.y, bbox_min_local.z, 0.0f);
-      glm::vec4 bbox_max_global = model * glm::vec4(bbox_max_local.x, bbox_max_local.y, bbox_max_local.z, 0.0f);
-      bbox_min                  = glm::vec3(bbox_min_global.x, bbox_min_global.y, bbox_min_global.z);
-      bbox_min                  = glm::vec3(bbox_max_global.x, bbox_max_global.y, bbox_max_global.z);
-   };
-
-   AABB(glm::vec4 center_point, glm::vec4 size) {
-      vec4::p1   = center_point + size;
-      vec4::p2   = center_point - size;
-      bbox_min.x = std::min(p1.x, p2.x);
-      bbox_min.y = std::min(p1.y, p2.y);
-      bbox_min.z = std::min(p1.z, p2.z);
-      bbox_max.x = std::max(p1.x, p2.x);
-      bbox_max.y = std::max(p1.y, p2.y);
-      bbox_max.z = std::max(p1.z, p2.z);
-   };
-
-
-   glm::vec3 get_min() { return bbox_min; };
-   glm::vec3 get_max() { return bbox_max };
+AABB::AABB(glm::vec3 bbox_min_local, glm::vec3 bbox_max_local, glm::mat4 model, int id) {
+   glm::vec4 bbox_min_global = model * glm::vec4(bbox_min_local.x, bbox_min_local.y, bbox_min_local.z, 0.0f);
+   glm::vec4 bbox_max_global = model * glm::vec4(bbox_max_local.x, bbox_max_local.y, bbox_max_local.z, 0.0f);
+   bbox_min                  = glm::vec3(bbox_min_global.x, bbox_min_global.y, bbox_min_global.z);
+   bbox_min                  = glm::vec3(bbox_max_global.x, bbox_max_global.y, bbox_max_global.z);
+   id                        = id;
 };
 
+AABB::AABB(glm::vec4 center_point, glm::vec4 size, int id) {
+   glm::vec4 p1 = center_point + size;
+   glm::vec4 p2 = center_point - size;
+   bbox_min.x   = std::fmin(p1.x, p2.x);
+   bbox_min.y   = std::fmin(p1.y, p2.y);
+   bbox_min.z   = std::fmin(p1.z, p2.z);
+   bbox_max.x   = std::fmax(p1.x, p2.x);
+   bbox_max.y   = std::fmax(p1.y, p2.y);
+   bbox_max.z   = std::fmax(p1.z, p2.z);
+   id           = id;
+};
+
+bool AABB::operator<(const AABB &other) const { return id < other.id; };
+
+glm::vec3 AABB::get_min() { return bbox_min; };
+glm::vec3 AABB::get_max() { return bbox_max; };
+
 bool AABB_to_AABB_intersec(AABB aabb1, AABB aabb2) {
-   glm::vec3 aabb_min_1 = aabb1.get_min();
-   glm::vec3 aabb_max_1 = aabb1.get_max();
-   glm::vec3 aabb_min_2 = aabb1.get_min();
+   glm::vec3 aMin = aabb1.get_min();
+   glm::vec3 aMax = aabb1.get_max();
+   glm::vec3 bMin = aabb2.get_min();
+   glm::vec3 bMax = aabb2.get_max();
 
    return (aMin.x <= bMax.x && aMax.x >= bMin.x) && (aMin.y <= bMax.y && aMax.y >= bMin.y) && (aMin.z <= bMax.z && aMax.z >= bMin.z);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,7 @@
 // Headers locais, definidos na pasta "include/"
 #include "utils.h"
 #include "matrices.h"
+#include "collisions.hpp"
 
 
 // Estrutura que representa um modelo geométrico carregado a partir de um
@@ -334,7 +335,8 @@ int main(int argc, char *argv[]) {
    float prev_time             = (float)glfwGetTime();
    float speed                 = 2.0f;
    glm::vec4 camera_position_c = glm::vec4(-0.75f, 0.75f, 0.75f, 1.0f);
-   glm::vec4 camera_aabb_size  = glm::vec4(0.75f, 0.75f, 0.75f, 1.0f);
+   glm::vec4 camera_aabb_size  = glm::vec4(2.0f, 2.0f, 2.0f, 1.0f);
+   std::map<AABB, bool> cam_collision_map;
 
    // Ficamos em um loop infinito, renderizando, até que o usuário feche a janela
    while (!glfwWindowShouldClose(window)) {
@@ -399,6 +401,7 @@ int main(int argc, char *argv[]) {
          camera_position_c  = glm::vec4(x, y, z, 1.0f);
          camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
       }
+      AABB cam_aabb(camera_position_c, camera_aabb_size, 0);
       // Computamos a matriz "View" utilizando os parâmetros da câmera para
       // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
       glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
@@ -472,21 +475,25 @@ int main(int argc, char *argv[]) {
       .*/
 
       model = Matrix_Translate(10.0f, -0.6f, 0.0f) * Matrix_Scale(0.03f, 1.0f, 10.0f);
+      AABB aabb_cube1(g_VirtualScene["Cube"].bbox_min, g_VirtualScene["Cube"].bbox_max, model, 1);
       glUniformMatrix4fv(g_model_uniform, 1, GL_FALSE, glm::value_ptr(model));
       glUniform1i(g_object_id_uniform, CUBE);
       DrawVirtualObject("Cube");
 
       model = Matrix_Translate(-10.0f, -0.6f, 0.0f) * Matrix_Scale(0.03f, 1.0f, 10.0f);
+      AABB aabb_cube2(g_VirtualScene["Cube"].bbox_min, g_VirtualScene["Cube"].bbox_max, model, 2);
       glUniformMatrix4fv(g_model_uniform, 1, GL_FALSE, glm::value_ptr(model));
       glUniform1i(g_object_id_uniform, CUBE);
       DrawVirtualObject("Cube");
 
       model = Matrix_Translate(0.0f, -0.6f, 10.0f) * Matrix_Scale(10.0f, 1.0f, 0.03f);
+      AABB aabb_cube3(g_VirtualScene["Cube"].bbox_min, g_VirtualScene["Cube"].bbox_max, model, 3);
       glUniformMatrix4fv(g_model_uniform, 1, GL_FALSE, glm::value_ptr(model));
       glUniform1i(g_object_id_uniform, CUBE);
       DrawVirtualObject("Cube");
 
       model = Matrix_Translate(0.0f, -0.6f, -10.0f) * Matrix_Scale(10.0f, 1.0f, 0.03f);
+      AABB aabb_cube4(g_VirtualScene["Cube"].bbox_min, g_VirtualScene["Cube"].bbox_max, model, 4);
       glUniformMatrix4fv(g_model_uniform, 1, GL_FALSE, glm::value_ptr(model));
       glUniform1i(g_object_id_uniform, CUBE);
       DrawVirtualObject("Cube");
@@ -496,7 +503,30 @@ int main(int argc, char *argv[]) {
       glUniform1i(g_object_id_uniform, PLANE);
       DrawVirtualObject("the_plane");
 
+      cam_collision_map[aabb_cube1] = AABB_to_AABB_intersec(cam_aabb, aabb_cube1);
+      cam_collision_map[aabb_cube2] = AABB_to_AABB_intersec(cam_aabb, aabb_cube2);
+      cam_collision_map[aabb_cube3] = AABB_to_AABB_intersec(cam_aabb, aabb_cube3);
+      cam_collision_map[aabb_cube4] = AABB_to_AABB_intersec(cam_aabb, aabb_cube4);
+
+      glm::vec3 min1 = aabb_cube1.get_min();
+      glm::vec3 max1 = aabb_cube1.get_max();
+      printf("Cube 1 Min: (%f, %f, %f)\n", min1.x, min1.y, min1.z);
+      printf("Cube 1 Max: (%f, %f, %f)\n", max1.x, max1.y, max1.z);
+
+      if (cam_collision_map[aabb_cube1])
+         printf("Colision cube 1\n");
+
+      if (cam_collision_map[aabb_cube2])
+         printf("Colision cube 2\n");
+
+      if (cam_collision_map[aabb_cube3])
+         printf("Colision cube 3\n");
+
+      if (cam_collision_map[aabb_cube4])
+         printf("Colision cube 4\n");
+
       TextRendering_ShowEulerAngles(window);
+
 
       // Imprimimos na informação sobre a matriz de projeção sendo utilizada.
       TextRendering_ShowProjection(window);
